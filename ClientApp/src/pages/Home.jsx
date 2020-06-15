042b076b302acf920d7b043b0361efe36d419b72
Michael Keough <keoughm21@gmail.com>
2020-06-15 13:56:18 -0400

    started homepage

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -4,7 +4,19 @@ export function Home() {
   return (
     <>
       <h1>Union Park Lawn</h1>
-      <h2>Family owned and operated.</h2>
+      <h2>Established in Wilmington, Delaware in 2008</h2>
+      <h3>Top Rated Lawn Care & Landscaping Services</h3>
+      <h3>Services Available</h3>
+      <ul>
+        <li>Tree Removal</li>
+        <li>Lawn Care</li>
+        <li>Landscaping</li>
+        <li>Hardscaping</li>
+        <li>Snow Cleaning</li>
+        <li>Aeration</li>
+        <li>Power Washing</li>
+        <li>Gutter cleaning</li>
+      </ul>
     </>
   )
 }
